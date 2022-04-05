3d1f97d0ec2e71c9677b34e38992560c6ee20537
radheys809 <radheys908@gmail.com>
2022-04-05 23:39:12 +0530

    first commit

---
+++ src/main/java/io/cloud/AllIsAboutCloudApplication.java
@@ -0,0 +1,13 @@
+package io.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllIsAboutCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllIsAboutCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/cloud/AllIsAboutCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllIsAboutCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
